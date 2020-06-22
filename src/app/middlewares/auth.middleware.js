0f8c3ed30f899b132404f0d221fce39f06b90568
Azat <mkhitaryan103@gmail.com>
2020-06-22 19:09:20 +0400

    auth middleware checking user

--- src/app/middlewares/auth.middleware.js
+++ src/app/middlewares/auth.middleware.js
@@ -1,11 +1,29 @@
 const AppError = require("../utils/AppError");
 const responseManager = require('../utils/ResponseManager');
+const TokenManager = require("../utils/TokenManager");
+const User = require('../models/user.model');
 
 function auth(req, res, next) {
     const responseHandler = responseManager.getResponseHandler(res);
-
-    if(!req.headers.authorization) {
+    let token = req.headers.authorization; 
+    if(!token) {
         responseHandler.onError(new AppError('No access token!'));
+    } else {
+        try {
+            const decoded = await TokenManager.decode(token, env.env.JWT_SECRET);
+            if (decoded.uid) {
+                const user = await User.findOne({_id: decoded.uid});
+                if (user) {
+                    next();
+                } else {
+                    return responseHandler.onError(new AppError("Invalid credentials", 401));
+                }
+            } else {
+                return responseHandler.onError(new AppError("Invalid Access Token.", 403));
+            }
+        } catch (e) {
+            return responseHandler.onError(new AppError("Invalid Access Token.", 401));
+        }
     }
     next();
 }
